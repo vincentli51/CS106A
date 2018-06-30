a060a18a1f0a54faa2718b217bd22aa65d901e64
Vincent Li <vincentl@eschs.org>
2018-06-30 16:09:10 -0400

    CheckerboardKarel Finished

--- Assignment1/CheckerboardKarel.java
+++ Assignment1/CheckerboardKarel.java
@@ -11,68 +11,89 @@ import stanford.karel.*;
 
 public class CheckerboardKarel extends SuperKarel {
 
+	/**
+	 * Karel fill the world with beepers in a checkerboard pattern 
+	 * @precondition Karel is facing East on the bottom left corner
+	 * @postcondition Karel is on the same avenue, either on the top facing facing west 
+	 * or on the bottom facing south
+	 */
 	public void run() {
+		putBeeper();
 		while (frontIsClear()) {
-			while (frontIsClear()) {
-				oneLine();
-				}		
-			if (frontIsBlocked()) {
+			oneLine();
+			turnRight();
+			if (frontIsClear()) {
+				move();
 				turnRight();
-				if (frontIsClear()) {
-					move();
-					turnRight();
-				}	
-				if (frontIsClear()) {
-					move();
-				}
 			}
 			if (frontIsClear()) {
+				move();
+				if (noBeepersPresent()) {
+					putBeeper();
+				}
 				oneLine();
 			}
 			if (noBeepersPresent()) {
 				turnRight();
-				if (frontIsClear()) {
-					move();
-					turnRight();
-				}	
+			}
+			if (frontIsClear()) {
+				move();
+				turnRight();
 			}
 		}
-		if (frontIsBlocked() && noBeepersPresent()) {
-			turnLeft();
-			if (frontIsClear()) {
-				oneLine();
-			}
+		turnLeft();
+		if ((frontIsClear()) && (beepersPresent())) {	
+			oneLine();
 		}
 	}
-	
-	
 
+	/**
+	 * Karel fills a stret with beepers in a pattern.
+	 * @precondition Karel is facing the direction of a street 
+	 * @postcondition Karel is in the same spot she started in, but looking the opposite direction
+	 */
 	private void oneLine() {
-			while (frontIsClear()) {
-				movingForward();
-			}
-			
-			if (frontIsBlocked()) {
-				turnAround();
-			}
+		while (frontIsClear()) {
+			movingForward();
+		}
+		turnAround();
+		if (frontIsClear()) {
+			move();
+		}
+		checkOK();
+		while (frontIsClear()) {
+			move();
+		}
+	}
+
+	/**
+	 * Karel checks if the pattern is correct by taking a step back to the end of the street and 
+	 * checking whether a beeper belong in the spot
+	 * @precondition Karel is facing the opposite direction in which it has filled the street
+	 * @postcondition Karel is facing the same direction, but one corner back
+	 */
+	private void checkOK() {
+		if (noBeepersPresent() && frontIsClear()) {
+			turnAround();
 			if (frontIsClear()) {
 				move();
 			}
-			if (noBeepersPresent() && frontIsClear()) {
-				turnAround();
-				move();
+			if (noBeepersPresent()) {
 				putBeeper();
-				turnAround();
-			} else {
+			}
+			turnAround();
+		} else {
+			if (frontIsClear()) {
 				move();
 			}
-			while (frontIsClear()) {
-				move();
-			}
+		}
 	}
-	
-	
-	
+
+	/**
+	 * Karel is placing a beeper then moving twice, if allowed, to achieve the checkboard pattern
+	 * @precondition Karel is facing a direction
+	 * @postcondition Karel is at max 2 avenues down in the direction it was facing
+	 */
 	private void movingForward() {
 		while (noBeepersPresent()) {
 			putBeeper();
@@ -83,87 +104,6 @@ public class CheckerboardKarel extends SuperKarel {
 		if (frontIsClear()) {
 			move();
 		}
-		
-
 	}
-		
-		
-		
 }
-	
-	
-	
-	
-	
-	
-	
-	
-	
-
-	
-	
-	
-	
-	
-		
-	
-	
-//	public void run() {
-//		for (int i=0;i<4;i++) {
-//			doubleStreets();
-//		}
-//	}
-//	
-//	private void doubleStreets() {
-//		oddStreets();
-//		evenStreets();
-//	}
-//	
-//	private void oddStreets() {
-//		putBeeper();
-//		move();
-//		move();
-//		putBeeper();
-//		move();
-//		move();
-//		putBeeper();
-//		move();
-//		move();
-//		putBeeper();
-//		turnAround();
-//		for (int i=0; i<6;i++) {
-//			move();
-//		}
-//		turnRight();
-//		move();
-//	}
-//		
-//	private void evenStreets() {
-//		turnRight();
-//		move();
-//		putBeeper();
-//		move();
-//		move();
-//		putBeeper();
-//		move();
-//		move();
-//		putBeeper();
-//		move();
-//		move();
-//		putBeeper();
-//		turnAround();
-//		for (int i=0; i<7;i++) {
-//			move();
-//		}
-//		turnRight();
-//		if (frontIsClear()) {
-//			move();
-//			turnRight();
-//		}
-//	}
-//}
-//	
-
-
-	
 
